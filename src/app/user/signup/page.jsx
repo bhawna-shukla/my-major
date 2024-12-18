3f8e33eed48ef78d62a111b1be8b5cd1fc40bee8
Bhawna Shukla <shuklabhawna81@gmail.com>
2024-12-18 10:38:42 +0530

    18/dec

--- src/app/user/signup/page.jsx
+++ src/app/user/signup/page.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div className=' min-h-screen  flex items-center justift-center '>
+    <div className=''>
 
-<div className="flex w-full  max-w-sm  mx-auto overflow-hidden bg-sky-500 border-2 border-slate-100 rounded-lg shadow-2xl shadow-blue-400 lg:max-w-5xl">
+<div className="flex w-full mt-24 max-w-sm mx-auto overflow-hidden bg-sky-500  rounded-lg shadow-lg shadow-blue-950 lg:max-w-4xl">
   <div
-    className="hidden bg-cover lg:block lg:w-2/3"
+    className=" hidden bg-cover lg:block lg:w-1/2"
     style={{
       backgroundImage:
-        'url("https://static.vecteezy.com/system/resources/thumbnails/011/345/227/small_2x/teenage-showing-how-to-sign-up-3d-cartoon-character-illustration-png.png")'
+        'url("https://png.pngtree.com/png-vector/20220521/ourmid/pngtree-man-and-woman-using-smartphones-vector-png-image_4656602.png")'
     }}
   />
   <div className="w-full mt-7  px-6 py-8 md:px-8 lg:w-1/2">
@@ -21,103 +21,66 @@ const Signup = () => {
         alt=""
       /> */}
     </div>
-    <p className="pb-3  text-3xl font-sans font-bold text-center text-white ">
-      Create Your Account 
+    <p className="mt-3 text-xl font-sans font-bold text-center text-white ">
+      Create your Account
     </p>
-    {/* <a
-      href="#"
-      className="flex items-center justify-center mt-4 text-gray-600 "
-    >
-      <div className="px-4 py-2">
-        <svg className="w-6 h-6" viewBox="0 0 40 40">
-          <path
-            d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-            fill="#FFC107"
-          />
-          <path
-            d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-            fill="#FF3D00"
-          />
-          <path
-            d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-            fill="#4CAF50"
-          />
-          <path
-            d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-            fill="#1976D2"
-          />
-        </svg>
-      </div>
-      <span className="w-5/6 px-4 py-3 font-bold text-center">
-        Sign in with Google
-      </span>
-    </a> */}
-    {/* <div className="flex items-center justify-between mt-4">
-      <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4" />
-      <a
-        href="#"
-        className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
-      >
-        or login with email
-      </a>
-      <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4" />
-    </div> */}
-    <div className="mt-1">
-      <label
-        className="block mb-2 text-md font-medium font-serif text-white"
-        htmlFor="LoggingEmailAddress"
-      >
-        First Name
-      </label>
-      <input
-        id="LoggingEmailAddress"
-        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg "
-        type="text"
-      />
-    </div>
+   
+ 
     <div className="mt-7">
       <label
         className="block mb-2 text-md font-medium font-serif text-white"
         htmlFor="LoggingEmailAddress"
       >
-        Last Name
+        First_Name
       </label>
       <input
-        id="LoggingEmailAddress"
-        className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg "
+        id="firstname"
+        className="block w-full px-4 py-2 text-gray-900 bg-white border rounded-lg "
         type="text"
+        placeholder='Enter your First Name'
       />
     </div>
-    <div className="mt-7">
+
+    <div className="mt-2">
       <label
         className="block mb-2 text-md font-medium font-serif text-white"
         htmlFor="LoggingEmailAddress"
       >
-        Email Address
+        Last_Name
       </label>
       <input
-        id="LoggingEmailAddress"
+        id="lastname"
+        className="block w-full px-4 py-2 text-gray-900 bg-white border rounded-lg "
+        type="text"
+        placeholder='Enter your Last Name'
+      />
+    </div>
+    <div className="mt-2">
+      <label
+        className="block mb-2 text-md  font-medium font-serif text-white"
+        htmlFor="LoggingEmailAddres"
+      >
+        Email_Address
+      </label>
+      <input
+        id="email"
         className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg "
         type="email"
+        placeholder='Enter your Email'
       />
     </div>
     <div className="mt-4">
-      <div className="flex justify-between">
+     
         <label
-          className="block mb-2 text-md font-serif text-white "
+          className="block mb-2 text-md  font-serif text-white "
           htmlFor="loggingPassword"
         >
           Password
         </label>
-        <a
-          href="#"
-          className="text-xs text-white underline hover:text-blue-900 "
-        >
-          Forget Password?
-        </a>
-      </div>
+       
+    
       <input
-        id="loggingPassword"
+        id="password"
         className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg "
         type="password"
       />
